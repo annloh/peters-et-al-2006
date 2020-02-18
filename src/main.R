c588c4d2c696e99c26970ea44f0f118ba60a7a09
Anna Lohmann <anna@lohmann-web.net>
2020-02-18 15:32:09 +0100

    :star: Integrate data:generation

---
+++ src/main.R
@@ -0,0 +1,25 @@
+# Code pertaining to the replication attempt of
+# Peters et al 2006
+
+source('dependencies.R')
+source('utils.R') # load utility fuctions
+source('scenarios.R') # load df with simulation scenarios
+
+
+job_id <- 1
+range <- 135
+
+purrr::pmap(scenarios[range,], ~{
+  start_time <-  Sys.time()
+  generate_ma(job_id = job_id,
+              scenario_id = ..1, #inputs first column
+              bias_type = ..2,
+              bias_percentage = ..3,
+              bias_strength = ..4,
+              odds_ratio = ..5,
+              heterogeneity = ..6,
+              ma_size = ..7,
+              prob_cg_distr = prob_cg_distr)
+})
+
+
